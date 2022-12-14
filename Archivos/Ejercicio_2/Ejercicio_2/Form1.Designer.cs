0b8b62902b3539a06a020cec775914c37363a0fc
Andres Licona <andreslicona27@icloud.com>
2022-12-14 11:09:11 +0100

    advances

--- Archivos/Ejercicio_2/Ejercicio_2/Form1.Designer.cs
+++ Archivos/Ejercicio_2/Ejercicio_2/Form1.Designer.cs
@@ -28,12 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPhrase = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lbPhrasesCount = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(26, 36);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(331, 23);
+            this.txtPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Insert your path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Insert the searching phrase";
+            // 
+            // txtPhrase
+            // 
+            this.txtPhrase.Location = new System.Drawing.Point(26, 91);
+            this.txtPhrase.Name = "txtPhrase";
+            this.txtPhrase.Size = new System.Drawing.Size(331, 23);
+            this.txtPhrase.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(257, 183);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Start Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lbPhrasesCount
+            // 
+            this.lbPhrasesCount.FormattingEnabled = true;
+            this.lbPhrasesCount.ItemHeight = 15;
+            this.lbPhrasesCount.Location = new System.Drawing.Point(407, 36);
+            this.lbPhrasesCount.Name = "lbPhrasesCount";
+            this.lbPhrasesCount.Size = new System.Drawing.Size(316, 169);
+            this.lbPhrasesCount.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(407, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "The phrase is in:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 183);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(206, 19);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Distinguish between capital letters";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 23);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Manage extensions";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(749, 218);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbPhrasesCount);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPhrase);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Find Your Phrase";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtPath;
+        private Label label1;
+        private Label label2;
+        private TextBox txtPhrase;
+        private Button btnSearch;
+        private ListBox lbPhrasesCount;
+        private Label label3;
+        private CheckBox checkBox1;
+        private TextBox textBox1;
+        private Label label4;
     }
 }
--- Archivos/Ejercicio_2/Ejercicio_2/Form1.cs
+++ Archivos/Ejercicio_2/Ejercicio_2/Form1.cs
@@ -2,9 +2,50 @@ namespace Ejercicio_2
 {
     public partial class Form1 : Form
     {
+        DirectoryInfo searchingDir;
+        string searchingPhrase;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string path = txtPath.Text.Trim();
+            searchingPhrase = txtPhrase.Text;
+
+            try
+            {
+                if (path[0] == '%' && path[path.Length - 1] == '%')
+                {
+                    string variable = path.Substring(1, path.Length - 2);
+                    path = Environment.GetEnvironmentVariable(variable);
+                }
+
+                searchingDir = new DirectoryInfo(path);
+                StartSearch(searchingPhrase);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("You dont have permision to access this path informaiton", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("You have to add a valid path", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("That directory does not exist", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There has been a problem with the path", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void StartSearch(string phrase)
+        {
+            
         }
     }
 }
